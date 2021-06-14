04fb99eae68d15a3ad133157a1362ca77e708ef4
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-06-14 17:51:33 -0500

    Cambios hechos (necesita reestructurar)

--- Project 3/Herz.cpp
+++ Project 3/Herz.cpp
@@ -23,9 +23,79 @@ void Herz::pickItem( Item item)
         contInventory++;
     }
 }
-
-void Herz::increaseHp( Herz herz)
+ 
+// Herz ganará 5 puntos de vida máxima por cada pelea que gane
+void Herz::increaseHp(  )
 {
-    herz.hp++;
+    hpMax += 5;
 }
 
+
+int Herz::getHpMax()
+{
+    return hpMax;
+}
+
+
+void Herz::useItem( string code,int posInventory )
+{
+
+    // Entra a la poción de vida
+    if( code == "008f39" ) 
+    {
+        /* 
+            Si la vida del jugador sumada la poción es mayor a la vida máxima, Herz recuperará la diferencia
+            entre la vida que tenga y la vida máxima. Si el jugador tiene los puntos de vida máximos y usa la 
+            poción, no se le sumará ningún punto de vida y perderá la poción
+        */
+        if( ( hp + 100 ) >= hpMax )
+        {   
+            int sum = hp - hpMax;
+            hp += sum;
+        }
+        
+        // Si la vida del jugador sumada la poción es menor a la vida máxima, suma 100 puntos de vida
+        else if( ( hp + 100 ) < hpMax ) 
+        {
+            hp += 100;
+        }
+    }
+
+
+    // Entra a la poción de daño
+    else if( code == "FF0000" )
+    {
+        dmg += 10;      // El jugador obtiene 10 puntos de daño adicionales
+    }
+
+
+    // Entra a la poción de daño instantaneo 
+    else if( code == "ff8000" )
+    {
+        // AQUI HACE DAÑO AL ENEMIGO PERO DEBE DE ESTAR EN COMBATE
+    }
+    
+
+    // Entra a la poción de escape
+    else if( code == "572364" )
+    {   
+        /* 
+            Variable entera cuyo número será asignado entre 0 y 1 por medio del azar.
+            Si sale 1: La posción de escape funcionará
+            Si sale 0: La poción de escape hará que Herz pierda 10 puntos de vida máximos
+        */
+        int choice;
+        srand( time ( NULL ) );
+        choice = rand() % 2;
+
+        if( choice == 0 )
+        {
+            hpMax -= 10;    
+        }
+        else
+        {
+            // AQUI SERVIRÁ PARA QUE HERZ ESCAPE DE UNA BATALLA
+        }
+    }
+
+}
--- Project 3/Herz.hpp
+++ Project 3/Herz.hpp
@@ -2,26 +2,35 @@
 #define HERZ_H
 
 #include <iostream>
+#include <string>
 
 #include "Item.hpp"
+#include "Potion.hpp"
+#include "HealthPotion.hpp"
+#include "PowerPotion.hpp"
+#include "EscapePotion.hpp"
+#include "InstantDmgPotion.hpp"
 #include "Character.hpp"
 
 using namespace std;
 
 class Herz : public Character 
 {
-    protected:
+    private:
+        int hpMax = hp;     // Vida que será incrementada con poción
         class Item inventory[ 10 ];     // Inventario de Herz
-        int contInventory = 0;
+        int contInventory = 0;      // Sirve para saber cuantas posiciones del inventario de Herz están llenas
 
     public:
         Herz();
-        Herz( int, int );   // Parámetros que recibe: vida, daño
+        Herz( int, int );   // Parámetros que recibe: vidaMax, daño
         ~Herz();
         
         void pickItem( Item );    // Levantar item del suelo
-        void useItem( Item );     // Usar item 
-        void increaseHp( Herz );  // Cuando Herz esté en combate, aumenta sus puntos de vida
+        void useItem( string, int );     // Parámetros strin para un código y numero de posicion de dicho item en el inventario
+        void increaseHp( );  // Cuando Herz esté en combate, aumenta sus puntos de vida
+        int checkInventory( );   // Revisa el inventario para saber si está o no, lleno
+        int getHpMax( );
 
 };
 
