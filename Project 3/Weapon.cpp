561eb1f57f17d7b26321efe97744143121ed61ff
CristianRiascos <66748497+CristianRiascos@users.noreply.github.com>
2021-06-14 17:54:02 -0500

    Método añadido

--- Project 3/Weapon.cpp
+++ Project 3/Weapon.cpp
@@ -5,3 +5,8 @@ Weapon::Weapon( int dmg, int endurance )
     this->dmg = dmg;
     this->endurance = endurance;
 }
+
+int Weapon::getDmg()
+{
+    return dmg;
+}
--- Project 3/Weapon.hpp
+++ Project 3/Weapon.hpp
@@ -8,9 +8,12 @@ class  Weapon : public Item
     protected:
         int dmg;
     public:
+        Weapon() = default;
         Weapon( int, int );     // Parametros que recibe: daño, resistencia
         ~Weapon();
         
+        int getDmg();
+        virtual void attack() = 0;
 };
 
 #endif
